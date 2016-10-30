7e1190be00d6d136d02cccb9d49068d1d79bd185
lchsk <mjlechowski@gmail.com>
2016-10-30 11:13:59 +0000

    Remove weather from xmobar

--- xmobar.hs
+++ xmobar.hs
@@ -5,8 +5,8 @@ Config {
        , fgColor = "#c8c8c8"
        , position = BottomW L 100
        , commands = 
-	[ Run Weather "EGLL" ["-t"," <tempC>C","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
-          , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+        [
+          Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
           , Run Memory ["-t","Mem: <usedratio>%"] 10
           , Run Swap [] 10
           , Run Date "%a %b %_d %H:%M" "date" 10
@@ -31,4 +31,4 @@ Config {
 	] 
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpufreq% | %cpu% | %memory% | %swap% |%EGLL% %date%"
+       , template = "%StdinReader% }{ %cpufreq% | %cpu% | %memory% | %swap% | %date%"
