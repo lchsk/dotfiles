8c957edbb0619665a12eeb141a0e8d05a4a64ee6
Maciej Lechowski <maciej.lechowski@geneity.co.uk>
2017-05-24 09:59:00 +0100

    List all active connections in dzen bar

--- scripts/net.sh
+++ scripts/net.sh
@@ -1,6 +1,6 @@
 conn_name () {
 	# netctl list | grep -E -o "\* ([a-zA-Z0-9]+)" | sed -e "s/* //"
-	ip link | grep "state UP" | cut -f2 -d: | awk '{print $1}'
+	ip link | grep "state UP" | cut -f2 -d: | awk '{print $1}' | xargs
 }
 
 conn_name
