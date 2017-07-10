f025de69a3647d25d69c50faa25944b3aba00200
lchsk <mjlechowski@gmail.com>
2017-07-10 16:12:56 +0100

    Show only the first connection

--- scripts/net.sh
+++ scripts/net.sh
@@ -1,6 +1,6 @@
 conn_name () {
 	# netctl list | grep -E -o "\* ([a-zA-Z0-9]+)" | sed -e "s/* //"
-	ip link | grep "state UP" | cut -f2 -d: | awk '{print $1}' | xargs
+	ip link | grep "state UP" | cut -f2 -d: | awk '{print $1}' | head -n 1
 }
 
 conn_name
