88383b85b38c7dbc84f1fcfbb58cee26515128c2
lchsk <mjlechowski@gmail.com>
2019-03-25 21:34:07 +0000

    Use newer version of conky config

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,1 +1,1 @@
-amixer -D pulse get Master | grep -E -o "([0-9]{1,3}%).+?(on|off)" | sed -r -e "s/].*?\[/ /" | sed -e "s/off/mute/" | sed -e "s/ on//" | uniq
+amixer -D pulse get Master | grep -E -o "([0-9]{1,3}%).+?(on|off)" | sed -r -e "s/].*?\[/ /" | sed -e "s/off/mute/" | sed -e "s/ on//" | uniq | head -n1
